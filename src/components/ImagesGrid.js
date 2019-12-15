d382f367037e7f1a8cc0b6a96a3eafcbb28c7a98
Daniel Alejandro Cast <lexcast13@gmail.com>
2019-12-14 20:47:25 -0600

    Virtualize images grid for better performance

--- src/components/ImagesGrid.js
+++ src/components/ImagesGrid.js
@@ -1,11 +1,61 @@
 import React from "react";
 import ImagePixels from "./ImagePixels";
 import PalettePixels from "./PalettePixels";
+import { List, AutoSizer } from "react-virtualized";
 
 const ImagesGrid = ({ images, selectedPalette, colorsImage, PRICE }) => {
+  const list = Object.keys(images);
+
+  const imageRenderer = ({ key, index, style }) => {
+    const id = list[index];
+    const image = images[id];
+
+    return (
+      <div
+        key={key}
+        style={style}
+        className="flex w-1/2 p-2 cursor-pointer hover:bg-gray-800"
+      >
+        <div className="w-20 flex items-center justify-center">
+          <img
+            src={image.url}
+            alt={id}
+            style={{
+              imageRendering: "pixelated",
+              width: image.width + "px",
+              height: image.height + "px"
+            }}
+          />
+        </div>
+        <div className="w-20 flex items-center justify-center">
+          <ImagePixels
+            pixels={image.pixels}
+            palette={selectedPalette ? colorsImage[id].map : null}
+            width={image.width}
+            height={image.height}
+            size={1}
+          />
+        </div>
+        {selectedPalette && (
+          <>
+            <div className="w-20 flex items-center justify-end text-sm">
+              {colorsImage[id].total}
+            </div>
+            <div className="w-20 flex items-center justify-end text-sm">
+              ${(colorsImage[id].total * PRICE).toFixed(2)}
+            </div>
+            <div className="flex-1 flex flex-wrap items-center justify-center px-2">
+              <PalettePixels palette={colorsImage[id].count} useKey />
+            </div>
+          </>
+        )}
+      </div>
+    );
+  };
+
   return (
-    <div className="p-3 flex flex-col items-center">
-      <div className="flex w-1/2 p-2 text-xs uppercase text-gray-500 bg-gray-800 rounded-t opacity-50">
+    <div className="p-3 flex w-1/2 m-auto flex-col items-center">
+      <div className="flex w-full p-2 text-xs uppercase text-gray-500 bg-gray-800 rounded-t opacity-50">
         <div className="w-20 flex items-center justify-center">Original</div>
         <div className="w-20 flex items-center justify-center">Result</div>
         <div className="w-20 flex items-center justify-end">Pixels</div>
@@ -16,48 +66,19 @@ const ImagesGrid = ({ images, selectedPalette, colorsImage, PRICE }) => {
           Palette
         </div>
       </div>
-      {Object.keys(images).map(key => {
-        return (
-          <div
-            key={key}
-            className="flex w-1/2 p-2 cursor-pointer hover:bg-gray-800"
-          >
-            <div className="w-20 flex items-center justify-center">
-              <img
-                src={images[key].url}
-                alt={key}
-                style={{
-                  imageRendering: "pixelated",
-                  width: images[key].width + "px",
-                  height: images[key].height + "px"
-                }}
-              />
-            </div>
-            <div className="w-20 flex items-center justify-center">
-              <ImagePixels
-                pixels={images[key].pixels}
-                palette={selectedPalette ? colorsImage[key].map : null}
-                width={images[key].width}
-                height={images[key].height}
-                size={1}
-              />
-            </div>
-            {selectedPalette && (
-              <>
-                <div className="w-20 flex items-center justify-end text-sm">
-                  {colorsImage[key].total}
-                </div>
-                <div className="w-20 flex items-center justify-end text-sm">
-                  ${(colorsImage[key].total * PRICE).toFixed(2)}
-                </div>
-                <div className="flex-1 flex flex-wrap items-center justify-center px-2">
-                  <PalettePixels palette={colorsImage[key].count} useKey />
-                </div>
-              </>
-            )}
-          </div>
-        );
-      })}
+      <div className="w-full">
+        <AutoSizer>
+          {({ width }) => (
+            <List
+              height={300}
+              width={width}
+              rowCount={list.length}
+              rowHeight={80}
+              rowRenderer={imageRenderer}
+            />
+          )}
+        </AutoSizer>
+      </div>
     </div>
   );
 };
